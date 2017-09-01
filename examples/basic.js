31479abd8fd3a85cdcb607d22645abed6e17ae1e
pqml <hugo.piquemal@gmail.com>
2017-09-01 03:44:01 +0200

    Add bin opts and binary prompter / cache

--- examples/basic.js
+++ examples/basic.js
@@ -2,7 +2,7 @@ const php = require('..')
 const server = php()
 
 server.on('start', data => {
-  console.log('Server started on ' + data.host + ':' + data.port)
+  console.log('Server started on http://' + data.host + ':' + data.port)
 })
 
 server.on('error', err => {
--- src/server.js
+++ src/server.js
@@ -1,11 +1,23 @@
+const fs = require('fs-extra')
+const path = require('path')
 const spawn = require('child_process').spawn
 const commandExists = require('command-exists')
 const getPort = require('./get-port')
 const callStable = require('./call-stable')
 const Emitter = require('events')
+const shColors = require('kool-shell/utils/colors')
+const sh = require('kool-shell')()
+  .use(require('kool-shell/plugins/log'), {
+    errorPrefix: '[PHP] ' + shColors.red('Error: ')
+  })
+  .use(require('kool-shell/plugins/exec'))
+  .use(require('kool-shell/plugins/input'))
+
+const CACHEPATH = path.join(__dirname, '..', '.bincache')
 
 const DEF_OPTS = {
-  bin: false,
+  bin: 'php',
+  promptBinary: false,
   host: '0.0.0.0',
   port: 35410,
   root: process.cwd(),
@@ -22,7 +34,7 @@ function server (opts) {
   let handler
 
   const api = new Emitter()
-  const restart = callStable(start, () => handleError('Php built-in server closes too often.'))
+  const restart = callStable(start, () => error('Php built-in server closes too often.'))
 
   api.start = opts.autorestart ? restart : start
   api.close = close
@@ -30,20 +42,75 @@ function server (opts) {
 
   return api
 
-  function log (msg, force) {
+  function log (msg = '', force) {
     if ((!opts.verbose && !force) || opts.quiet) return
-    process.stdout.write('[PHP] ' + msg)
+    sh.log('[PHP] ' + msg.toString('utf8').trim())
   }
 
-  function handleOut (data) {
+  function error (msg = '', force) {
+    msg = msg.toString('utf8').trim()
+    api.emit('error', msg)
+    if ((!opts.verbose && !force) || opts.quiet) return
+    sh.error('[PHP] ' + msg)
+  }
+
+  function handleOut (data = '') {
     if (closed && !started) return
     if (closed) return close()
     log(data)
   }
 
-  function handleError (data) {
-    handleOut(data)
-    api.emit('error', data.toString('utf8').trim())
+  function writeBin (bin) {
+    let filepath = bin
+    Promise.resolve()
+      .then(() => new Promise(resolve => {
+        fs.pathExists(filepath, (err, exists) => {
+          if (err) return resolve()
+          if (exists && !path.isAbsolute(filepath)) {
+            const absPath = path.join(process.cwd(), filepath)
+            validateBinary(absPath)
+              .then(() => { filepath = absPath })
+              .then(resolve)
+              .catch(resolve)
+          } else resolve()
+        })
+      }))
+      .then(() => fs.ensureFile(CACHEPATH))
+      .then(() => fs.writeFile(CACHEPATH, filepath))
+      .catch(err => {})
+  }
+
+  function validateBinary (bin) {
+    return new Promise((resolve, reject) => {
+      bin = bin.toString().trim();
+      Promise.resolve()
+        .then(() => { if (bin === '') reject('Empty path') })
+        .then(() => commandExists(bin)).catch(err => reject(bin + ' doesn\'t exist'))
+        .then(() => checkPhp(bin)).catch(err => reject(bin + ' is not a php binary.'))
+        .then(() => resolve(bin))
+    })
+  }
+
+  function checkPhp (bin) {
+    return new Promise((resolve, reject) => {
+      sh.silentExec(bin, ['-v'])
+        .then(out => {
+          const log = out.stdout.trim()
+          if (log.startsWith('PHP') || log.startsWith('php')) resolve()
+          else return Promise.reject()
+        })
+        .catch(reject)
+    })
+  }
+
+  function promptPhp () {
+    return sh.input('Valid path or command for php:', {
+      onSubmit: path => new Promise((resolve, reject) => {
+        validateBinary(path)
+          .then(bin => resolve(bin))
+          .catch(err => sh.error(err) && promptPhp().then(bin => resolve(bin)))
+      })
+    })
   }
 
   function startProcess () {
@@ -56,7 +123,7 @@ function server (opts) {
           }
           const addr = opts.host + ':' + resolvedPort
 
-          handler = spawn('php', ['-S', addr, '-t', opts.root])
+          handler = spawn(opts.bin, ['-S', addr, '-t', opts.root])
 
           handler.stdout.on('data', handleOut)
           handler.stderr.on('data', handleOut)
@@ -74,10 +141,31 @@ function server (opts) {
     closed = false
     if (started) return
 
-    commandExists('php')
-      .catch(() => Promise.reject(new Error('php is not installed on your system.')))
+    validateBinary(opts.bin)
+      .catch(err => {
+        return new Promise((resolve, reject) => {
+          Promise.resolve()
+            .then(() => fs.ensureFile(CACHEPATH))
+            .then(() => fs.readFile(CACHEPATH))
+            .then(res => res.toString().trim() === '' ? reject(err) : res)
+            .then(bin => validateBinary(bin))
+            .then(bin => { opts.bin = bin })
+            .then(resolve)
+            .catch(reject)
+        })
+      })
+      .catch(err => {
+        sh.error(err)
+        if (!opts.promptBinary) return Promise.reject()
+        return new Promise(resolve =>
+          promptPhp()
+            .then(bin => { opts.bin = bin })
+            .then(() => writeBin(opts.bin))
+            .then(resolve)
+        )
+      })
       .then(() => startProcess())
-      .catch(err => handleError(err))
+      .catch(err => error(err))
   }
 
   function close (code) {
