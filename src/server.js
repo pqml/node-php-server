9dfa7d2143f788a1f96b0ff5393d50cbbbff41d6
pqml <hugopiquemal@protonmail.com>
2021-06-18 15:49:00 +0200

    Fix for Big Sur

--- src/server.js
+++ src/server.js
@@ -5,7 +5,10 @@ const getPort = require('./get-port')
 const callStable = require('./call-stable')
 const Emitter = require('events')
 const sh = require('kool-shell/namespaced')('node-php-server')
-sh.setLogOptions({ globalPrefix: '[PHP] ' })
+
+sh.setLogOptions({
+  globalPrefix: '[PHP] '
+})
 
 const CACHEPATH = path.join(__dirname, '..', '.bincache')
 
@@ -21,7 +24,7 @@ const DEF_OPTS = {
   args: []
 }
 
-function server (opts) {
+function server(opts) {
   opts = Object.assign({}, DEF_OPTS, opts || {})
 
   let closed = false
@@ -37,35 +40,38 @@ function server (opts) {
       if (!properInit) ++currentPort
       start()
     },
-    () => error('Php built-in server closes too often.'),
-    { maxRetries: 20 }
+    () => error('Php built-in server closes too often.'), {
+      maxRetries: 20
+    }
   )
 
   api.start = start
   api.close = close
-  api.isStarted = function () { return started }
+  api.isStarted = function () {
+    return started
+  }
 
   return api
 
-  function log (msg = '', force) {
+  function log(msg = '', force) {
     if ((!opts.verbose && !force) || opts.quiet) return
     sh.log(msg.toString('utf8').trim())
   }
 
-  function error (msg = '', force) {
+  function error(msg = '', force) {
     msg = msg.toString('utf8').trim()
     api.emit('error', msg)
     if ((!opts.verbose && !force) || opts.quiet) return
     sh.error(msg)
   }
 
-  function handleOut (data = '') {
+  function handleOut(data = '') {
     if (closed && !started) return
     if (closed) return close()
     log(data)
   }
 
-  function writeBin (bin) {
+  function writeBin(bin) {
     let filepath = bin
     Promise.resolve()
       .then(() => new Promise(resolve => {
@@ -74,7 +80,9 @@ function server (opts) {
           if (exists && !path.isAbsolute(filepath)) {
             const absPath = path.join(process.cwd(), filepath)
             validateBinary(absPath)
-              .then(() => { filepath = absPath })
+              .then(() => {
+                filepath = absPath
+              })
               .then(resolve)
               .catch(resolve)
           } else resolve()
@@ -85,29 +93,37 @@ function server (opts) {
       .catch(err => {})
   }
 
-  function validateBinary (bin) {
+  function validateBinary(bin) {
     return new Promise((resolve, reject) => {
       bin = bin.toString().trim();
       Promise.resolve()
-        .then(() => { if (bin === '') reject('Empty path') })
+        .then(() => {
+          if (bin === '') reject('Empty path')
+        })
         .then(() => checkPhp(bin)).catch(err => reject(bin + ' is not a php binary.'))
         .then(() => resolve(bin))
     })
   }
 
-  function checkPhp (bin) {
+  function checkPhp(bin) {
     return new Promise((resolve, reject) => {
       sh.silentExec(bin, ['-v'])
         .then(out => {
-          const log = out.stdout.trim()
-          if (log.startsWith('PHP') || log.startsWith('php')) resolve()
-          else return Promise.reject()
+          const log = out.stdout.trim().toLowerCase();
+          if (log.includes('macos will not include php')) {
+            console.log('⚠️  Built-in PHP will be removed from future versions of macOS')
+            return resolve()
+          } else if (log.startsWith('php')) {
+            return resolve()
+          } else {
+            return Promise.reject()
+          }
         })
         .catch(reject)
     })
   }
 
-  function promptPhp () {
+  function promptPhp() {
     return sh.input('Valid path or command for php:', {
       onSubmit: path => new Promise((resolve, reject) => {
         validateBinary(path)
@@ -117,7 +133,7 @@ function server (opts) {
     })
   }
 
-  function startProcess () {
+  function startProcess() {
     return new Promise((resolve, reject) => {
       getPort(currentPort)
         .then(resolvedPort => {
@@ -140,14 +156,17 @@ function server (opts) {
           clearTimeout(startEventTimer)
           startEventTimer = setTimeout(() => {
             log('Server started on ' + opts.host + ':' + resolvedPort + '\n')
-            api.emit('start', { host: opts.host, port: resolvedPort })
+            api.emit('start', {
+              host: opts.host,
+              port: resolvedPort
+            })
           }, 500)
         })
         .catch(reject)
     })
   }
 
-  function start () {
+  function start() {
     closed = false
     if (started) return
 
@@ -159,7 +178,9 @@ function server (opts) {
             .then(() => fs.readFile(CACHEPATH))
             .then(res => res.toString().trim() === '' ? reject(err) : res)
             .then(bin => validateBinary(bin))
-            .then(bin => { opts.bin = bin })
+            .then(bin => {
+              opts.bin = bin
+            })
             .then(resolve)
             .catch(reject)
         })
@@ -169,16 +190,18 @@ function server (opts) {
         if (!opts.promptBinary) return Promise.reject()
         return new Promise(resolve =>
           promptPhp()
-            .then(bin => { opts.bin = bin })
-            .then(() => writeBin(opts.bin))
-            .then(resolve)
+          .then(bin => {
+            opts.bin = bin
+          })
+          .then(() => writeBin(opts.bin))
+          .then(resolve)
         )
       })
       .then(() => startProcess())
       .catch(err => error(err))
   }
 
-  function close (code) {
+  function close(code) {
     code = code !== undefined ? code : 0
     closed = true
     if (!handler || !started) return
@@ -187,7 +210,9 @@ function server (opts) {
     handler.kill()
     handler.removeAllListeners()
     log('Server closed.\n')
-    api.emit('close', { code: code })
+    api.emit('close', {
+      code: code
+    })
     if (opts.autorestart) restart()
   }
 }
